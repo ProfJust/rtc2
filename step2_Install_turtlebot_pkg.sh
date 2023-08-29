249ccf493735698015f7d50125cf4476c24ac9dc
ProfJust <olaf.just@w-hs.de>
2023-08-29 15:21:55 +0200

    -y

--- step2_Install_turtlebot_pkg.sh
+++ step2_Install_turtlebot_pkg.sh
@@ -1,14 +1,14 @@
 sudo apt install ros-humble-gazebo-* -y
-sudo apt install ros-humble-cartographer
-sudo apt install ros-humble-cartographer-ros
-sudo apt install ros-humble-navigation2
-sudo apt install ros-humble-nav2-bringup
+sudo apt install ros-humble-cartographer -y
+sudo apt install ros-humble-cartographer-ros -y
+sudo apt install ros-humble-navigation2 -y
+sudo apt install ros-humble-nav2-bringup -y
 
 source ~/.bashrc
 
-sudo apt install ros-humble-dynamixel-sdk
-sudo apt install ros-humble-turtlebot3-msgs
-sudo apt install ros-humble-turtlebot3
+sudo apt install ros-humble-dynamixel-sdk -y
+sudo apt install ros-humble-turtlebot3-msgs -y
+sudo apt install ros-humble-turtlebot3 -y
 
 echo 'export ROS_DOMAIN_ID=30 #TURTLEBOT3' >> ~/.bashrc
 source ~/.bashrc
