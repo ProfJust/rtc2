5926db7ca92a9e3fe20127ec5edce314bb433ba7
Prof. Just <olaf.just@w-hs.de>
2023-08-29 15:02:59 +0200

    turtlebot3_ws

--- step1_Install_ROS2_Hawskbill.sh
+++ step1_Install_ROS2_Hawskbill.sh
@@ -35,7 +35,7 @@ sudo apt update && sudo apt install -y \
    python3-pytest-rerunfailures
    
    
-   mkdir -p ~/ros2_humble/src
+   mkdir -p ~/turtlebot3_ws/src
 cd ~/ros2_humble
 vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
 
@@ -45,5 +45,5 @@ sudo rosdep init
 rosdep update
 rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
 
-cd ~/ros2_humble/
+cd ~/turtlebot3_ws/
 colcon build --symlink-install
