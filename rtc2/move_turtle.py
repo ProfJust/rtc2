1720b9fc2451fcbe9e8e7fe4ce48f0e9e0279591
ProfJust <olaf.just@w-hs.de>
2023-10-15 11:59:19 +0200

    Sunday Works

--- rtc2/move_turtle.py
+++ rtc2/move_turtle.py
@@ -1,17 +1,49 @@
 #!/usr/bin/env python3
+
+# move_turtle.py
+#   for rtc2
+#   by oj, 15.10.23
+#   Westfälische Hochschule - Campus Bocholt
+# -------------------------------------------
+# usage
+# $1 ros2 run turtlesim turtlesim_node 
+# $2 ros2 run rtc2 move_turtlesim
+# -------------------------------------------
+# First example to publish and subscribe
+# messages form a Robot (Turtlesim)
+# -------------------------------------------
 import rclpy
 from rclpy.node import Node
 
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+from turtlesim.msg import Pose
+
 import random
 
 class MoveTurtlesimNode(Node):
     def __init__(self):
         super().__init__("move_turtlesim_node")
         self.cmd_vel_publisher_ = self.create_publisher(
-            Twist, "turtle1/cmd_vel", 10)
+                                    Twist,
+                                    "turtle1/cmd_vel",
+                                    10)
         self.cmd_timer_ = self.create_timer(
-            0.1, self.publish_cmd)
+                            0.1,
+                            self.publish_cmd)
+        
+        self.subscription = self.create_subscription(
+                              Pose,
+                              'turtle1/pose',
+                              self.listener_callback,
+                              10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info(' Turtle is at Position x "%f"' % msg.x)
+        self.get_logger().info('                       y "%f"' % msg.y)
+        self.get_logger().info('                   theta "%f"' % msg.theta)
+
 
     def publish_cmd(self):
         vel = random.randint(0, 100) / 100
