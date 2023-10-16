2e013ccb30ca2096c6a9432e4eafafdf1e152a1d
ProfJust <olaf.just@w-hs.de>
2023-10-16 10:38:36 +0200

    andere Reihenfolge

--- launch/turtlesim_launch.py
+++ launch/turtlesim_launch.py
@@ -4,15 +4,15 @@ import launch_ros.actions
 def generate_launch_description():
     return launch.LaunchDescription([
         launch_ros.actions.Node(
-            package='turtlesim',
-            namespace='turtlesim1',
-            executable='turtlesim_node',
-            name='sim'
-        ),
-        launch_ros.actions.Node(
             package='rtc2',
             namespace='rtc2',
             executable='move_turtlesim',
             name='move_node'
         ),
+        launch_ros.actions.Node(
+            package='turtlesim',
+            namespace='turtlesim1',
+            executable='turtlesim_node',
+            name='sim'
+        ),        
     ])
