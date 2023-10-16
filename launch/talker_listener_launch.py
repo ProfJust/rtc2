f3e2289bc139e1658d4a151c601de7d4e76737eb
ProfJust <olaf.just@w-hs.de>
2023-10-16 12:33:46 +0200

    works

--- launch/talker_listener_launch.py
+++ launch/talker_listener_launch.py
@@ -1,15 +1,43 @@
-import launch
-import launch_ros.actions
+# talker_listener_launch.py
+#---------------------------------------------------------------------
+# 16.10.23 by OJ 
+# Westfälische Hochschule, Campus Bocholt, Ruhr TurtleBot Competition
+#---------------------------------------------------------------------
+# launches talker and listener at the same time
+# usage:
+# $ ros2 launch rtc2 talker_listener_launch.py
+# --------------------------------------------------------------------
 
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch_ros.actions import Node
+# from launch.event_handlers import OnExecutionComplete
+from launch.event_handlers import OnProcessStart
+  
 def generate_launch_description():
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='rtc2',
-            executable='talk',
-            name='talker'),
-        launch_ros.actions.Node(
-            package='rtc2',
-            executable='listen',
-            name='listener'),
-  ])
+     
+    turtlesim1 = Node(
+        package="rtc2",
+        executable="talk",
+        name='talker'
+    )
+ 
+    turtlesim2 = Node(
+        package="rtc2",
+        executable="listen",
+        name='listener'
+    )
+ 
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessStart(
+                target_action=turtlesim1,
+                on_start=[turtlesim2],
+            )
+        ),
+        turtlesim1
+    ])
 
+# for more information on launch files see
+# https://roboticscasual.com/tutorial-ros2-launch-files-all-you-need-to-know/
+# https://roboticscasual.com/tutorial-ros2-launch-files-all-you-need-to-know/#launch-event-handler-list
--- launch/turtlesim_launch.py
+++ launch/turtlesim_launch.py
@@ -1,3 +1,13 @@
+# turtlesim_launch.py
+#---------------------------------------------------------------------
+# 16.10.23 by OJ 
+# Westfälische Hochschule, Campus Bocholt, Ruhr TurtleBot Competition
+#---------------------------------------------------------------------
+# launches turtlesim and our move_node at the same time
+# usage:
+# $ ros2 launch rtc2 turtlesim_launch.py
+# --------------------------------------------------------------------
+
 from launch import LaunchDescription
 from launch.actions import RegisterEventHandler
 from launch_ros.actions import Node
@@ -27,3 +37,7 @@ def generate_launch_description():
         ),
         turtlesim1
     ])
+
+# for more information on launch files see
+# https://roboticscasual.com/tutorial-ros2-launch-files-all-you-need-to-know/
+# https://roboticscasual.com/tutorial-ros2-launch-files-all-you-need-to-know/#launch-event-handler-list
