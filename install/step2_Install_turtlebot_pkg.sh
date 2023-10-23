5a3268c6ad7fa96e3f9f57c7cc05cae4c12e9bc1
ProfJust <olaf.just@w-hs.de>
2023-10-23 12:20:12 +0200

    with burger gazebo

--- install/step2_Install_turtlebot_pkg.sh
+++ install/step2_Install_turtlebot_pkg.sh
@@ -8,13 +8,16 @@ sudo apt install ros-humble-cartographer -y
 sudo apt install ros-humble-cartographer-ros -y
 sudo apt install ros-humble-navigation2 -y
 sudo apt install ros-humble-nav2-bringup -y
-
-source ~/.bashrc
-
 sudo apt install ros-humble-dynamixel-sdk -y
 sudo apt install ros-humble-turtlebot3-msgs -y
 sudo apt install ros-humble-turtlebot3 -y
 
+cd ~/turtlebot3_ws/src/
+git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
+cd ~/turtlebot3_ws && colcon build --symlink-install
+
 echo 'export ROS_DOMAIN_ID=30 #TURTLEBOT3' >> ~/.bashrc
-echo "export PYTHONPATH=${PYTHONPATH}:~/turtlebot3_ws/src/rtc2/rtc2_dist_packages" >> ~/.bashrc
+echo 'export TURTLEBOT3_MODEL=burger' >> ~/.bashrc
+
+# echo "export PYTHONPATH=${PYTHONPATH}:~/turtlebot3_ws/src/rtc2/rtc2_dist_packages" >> ~/.bashrc
 source ~/.bashrc
