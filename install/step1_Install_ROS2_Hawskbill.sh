ba88333343a4ad1a557bf18750702bd9696babb2
ProfJust <olaf.just@w-hs.de>
2023-10-25 11:37:02 +0200

    new

--- install/step1_Install_ROS2_Hawskbill.sh
+++ install/step1_Install_ROS2_Hawskbill.sh
@@ -1,4 +1,8 @@
-# from http://docs.ros.org/en/humble/Installation/Alternatives/Ubuntu-Development-Setup.html
+# bisher from http://docs.ros.org/en/humble/Installation/Alternatives/Ubuntu-Development-Setup.html
+# doppelte Packete 
+# =>
+# am 25.10.23 geändert
+# https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debians.html
 
 locale  # check for UTF-8
 
@@ -9,33 +13,44 @@ export LANG=en_US.UTF-8
 
 locale  # verify settings
 
+# ensure that the Ubuntu Universe repository is enabled
 sudo apt install software-properties-common
 sudo add-apt-repository universe
 
+# add the ROS 2 GPG key with apt.
 sudo apt update 
 sudo apt upgrade
 sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
+#add the repository to your sources list
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
+# Install ROS 2 packages
+sudo apt update 
+sudo apt upgrade
 sudo apt install ros-humble-desktop
 sudo apt install ros-humble-ros-base
 sudo apt install ros-dev-tools
+# für die Praktika
 sudo apt install ros-humble-turtlesim
-# sudo apt install ros-humble-tutorials
+
+# Environment setup
 # Replace ".bash" with your shell if you're not using bash
 # Possible values are: setup.bash, setup.sh, setup.zsh
-source /opt/ros/humble/setup.bash
+
+mkdir -p ~/turtlebot3_ws/src
+echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+echo 'source ~/turtlebot3_ws/install/local_setup.bash' >> ~/.bashrc
 
 
-
+# Install development tools and ROS tools
 sudo apt update && sudo apt install -y \
   python3-flake8-docstrings \
   python3-pip \
   python3-pytest-cov
   
-  sudo apt install -y \
+sudo apt install -y \
    python3-flake8-blind-except \
    python3-flake8-builtins \
    python3-flake8-class-newline \
@@ -48,15 +63,19 @@ sudo apt update && sudo apt install -y \
 
   pip install -U setuptools
    
-mkdir -p ~/turtlebot3_ws/src
-cd ~/turtlebot3_ws
-vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
+# mkdir -p ~/turtlebot3_ws/src
+# cd ~/turtlebot3_ws
+
+#  ?????????????????? wo kommt das her?
+# vcs import --input https://raw.githubusercontent.com/ros2/ros2/humble/ros2.repos src
 
 sudo apt upgrade
-
 sudo rosdep init
 rosdep update
-rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
 
+# rosdep install --from-paths src --ignore-src -y --skip-keys "fastcdr rti-connext-dds-6.0.1 urdfdom_headers"
+
+source ~/.bashrc
 cd ~/turtlebot3_ws/
 colcon build --symlink-install
+source ~/turtlebot3_ws/install/local_setup.bash
