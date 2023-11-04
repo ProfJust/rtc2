6cc32da63631bb8a2a6549e318e93296937aab47
Prof. Just <olaf.just@w-hs.de>
2023-11-04 15:12:58 +0100

    ROS_LOCALHOST_ONLY

--- install/step2_Install_turtlebot_pkg.sh
+++ install/step2_Install_turtlebot_pkg.sh
@@ -19,6 +19,12 @@ cd ~/turtlebot3_ws && colcon build --symlink-install
 echo 'export ROS_DOMAIN_ID=30 #TURTLEBOT3' >> ~/.bashrc
 echo 'export TURTLEBOT3_MODEL=burger' >> ~/.bashrc
 
+# ROS_LOCALHOST_ONLY environment variable allows you to limit ROS 2 communication to localhost only.
+# This means your ROS 2 system, and its topics, services, and actions will not be visible to other computers
+# on the local network. Using ROS_LOCALHOST_ONLY is helpful in certain settings, 
+# such as classrooms, where multiple robots may publish to the same topic causing strange behaviors. 
+echo 'export ROS_LOCALHOST_ONLY=1' >> ~/.bashrc
+
 # echo "export PYTHONPATH=${PYTHONPATH}:~/turtlebot3_ws/src/rtc2/rtc2_dist_packages" >> ~/.bashrc
 source ~/.bashrc
 
