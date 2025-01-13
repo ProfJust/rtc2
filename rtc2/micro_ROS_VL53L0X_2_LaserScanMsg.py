16972ca1ebf3dbe51a414f65c7433c8d77e0501c
ProfJust <olaf.just@w-hs.de>
2025-01-13 12:44:41 +0100

    mit tf frame

--- rtc2/micro_ROS_VL53L0X_2_LaserScanMsg.py
+++ rtc2/micro_ROS_VL53L0X_2_LaserScanMsg.py
@@ -24,6 +24,13 @@ from rclpy.qos import ReliabilityPolicy, QoSProfile
 qos_profile = QoSProfile(depth=10)
 qos_profile.reliability = ReliabilityPolicy.BEST_EFFORT
 
+#ROS1 import tf
+from tf2_ros import TransformBroadcaster, TransformStamped
+import tf_transformations
+#ROS1 br = tf.TransformBroadcaster()
+import math
+     
+
 class micro_ROS_Node(Node):
     def __init__(self):
         super().__init__("move_turtlesim_node")
@@ -52,6 +59,26 @@ class micro_ROS_Node(Node):
         self.my_range1 = 0.0
         self.my_range2 = 0.0
 
+        self.br = TransformBroadcaster(self)
+        self.timer = self.create_timer(0.1, self.broadcast_transform_timer_cb)
+    
+    def broadcast_transform_timer_cb(self):
+           t = TransformStamped()
+           t.header.stamp = self.get_clock().now().to_msg()
+           t.header.frame_id = 'base_link'
+           t.child_frame_id = 'sensor_frame'
+           
+           t.transform.translation.x = 1.0
+           t.transform.translation.y = 0.0
+           t.transform.translation.z = 0.0
+           q = tf_transformations.quaternion_from_euler(0, 0, math.pi / 4)  # Roll, pitch, yaw
+           t.transform.rotation.x = q[0]
+           t.transform.rotation.y = q[1]
+           t.transform.rotation.z = q[2]
+           t.transform.rotation.w = q[3]
+           self.br.sendTransform(t)
+           print("sending transform VL53L0X_Left_link => base_link")
+
     def listener_callback1(self, msg):
         self.my_range1 = msg.data/1000 # msg in mm, range in m
         self.get_logger().info(' Sensor1 gives Position in m "%f"' % self.my_range1)
@@ -60,13 +87,21 @@ class micro_ROS_Node(Node):
         self.my_range2 = msg.data/1000 # msg in mm, range in m
         self.get_logger().info(' Sensor2 gives Position in m "%f"' % self.my_range2)
 
-    def publish_cmd(self):        
+    def publish_cmd(self):   
+        """br.sendTransform((-0.233, -0.43, 0.62),
+                         (tf.transformations.quaternion_from_euler(0, 0.80, 0.88, axes='sxyz')),
+                         #(tf.transformations.quaternion_from_euler(0, 0.88, 0.785, axes='sxyz')),
+                         rospy.Time.now(),
+                         "VL53L0X_Left_link",
+                         "base_link")
+        print("sending transform VL53L0X_Left_link => base_link")"""
+        
         msg = LaserScan()  # Leer msg instanzieren
         # ... und füllen
         # https://github.com/mikeferguson/ros2_cookbook/blob/main/rclpy/time.md
         t = self.get_clock().now()
         msg.header.stamp = t.to_msg()
-        msg.header.frame_id = "base_link"
+        msg.header.frame_id = "sensor_frame" #"base_link"
         msg.angle_min = -0.02 # start angle of the scan [rad] 
         msg.angle_max =  0.02 # start angle of the scan [rad]  
         msg.angle_increment = 0.01 # angular distance between measurements [rad]
@@ -86,6 +121,7 @@ class micro_ROS_Node(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = micro_ROS_Node()  # Instanzierung
+    # node2 = Tf2Broadcaster()
     rclpy.spin(node)
     rclpy.shutdown()
 
