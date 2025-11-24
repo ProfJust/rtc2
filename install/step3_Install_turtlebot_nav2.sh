4f04f014664d083d6264d1fe30ace1ae13a74a62
ProfJust <olaf.just@w-hs.de>
2025-11-24 13:50:00 +0100

    nav2 bringup added

--- install/step3_Install_turtlebot_nav2.sh
+++ install/step3_Install_turtlebot_nav2.sh
@@ -6,6 +6,7 @@ sudo apt install ros-jazzy-cartographer -y
 sudo apt install ros-jazzy-cartographer-ros -y
 sudo apt install ros-jazzy-nav2-map-server -y
 sudo apt install ros-jazzy-nav2-route -y
+sudo apt install ros-jazzy-nav2-bringup -y
 
 
 # cyclone DDS installieren (workaround not needed anymore)
